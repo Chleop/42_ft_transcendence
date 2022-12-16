d0f3e11ed4832501738b7c0da20cdf194059fd08
chleo <chleoproeschel@hotmail.fr>
2022-12-16 18:59:48 +0100

    added command prisma migrate deploy to docker file

--- backend/script/docker-entrypoint.dev.sh
+++ backend/script/docker-entrypoint.dev.sh
@@ -2,5 +2,8 @@
 
 npm i
 
+# ATTENTION cette commande ne prend pas en compte les changements effectues dans le schema !!!
+# Pour le developpement, remplacer par "npx prisma migrate dev"
+npx prisma migrate deploy 
+
 exec npm run start:dev
-# && npx prisma migrate dev (enfin j'aimerais bien mais ca marche pas)
