84a5c7145a87929fff122142ee36f51c90b77aea
chleo <chleoproeschel@hotmail.fr>
2022-12-16 19:43:56 +0100

    encore un peu de menage

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 
 import { PrismaModule } from './prisma/prisma.module';
-import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
-import { UserService } from './user/user.service';
 import { AuthService } from './auth/auth.service';
 
 @Module({
-  imports: [PrismaModule, UserModule, AuthModule],
+  imports: [PrismaModule, AuthModule],
 })
 export class AppModule {}
