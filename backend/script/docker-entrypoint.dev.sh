dbaa583bfaee1f90996e86200466acd91790d6b4
jodufour <jodufour@student.42.fr>
2022-12-16 20:02:51 +0100

    Restore personnal preferences to origin [OK]

--- backend/script/docker-entrypoint.dev.sh
+++ backend/script/docker-entrypoint.dev.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-npm install
+npm i
 
 # ATTENTION cette commande ne prend pas en compte les changements effectues dans le schema !!!
 # Pour le developpement, remplacer par "npx prisma migrate dev"
-npx prisma migrate deploy 
+npx prisma migrate deploy
 
 exec npm run start:dev
