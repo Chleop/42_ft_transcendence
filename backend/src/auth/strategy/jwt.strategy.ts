d9578fed17c7bcd5bb128a3c72a535bc32f9b3d3
Chleo PROESCHEL <cproesch@e3r10p9.42.fr>
2023-01-26 17:00:03 +0100

    updated jwt strategy

--- backend/src/auth/strategy/jwt.strategy.ts
+++ backend/src/auth/strategy/jwt.strategy.ts
@@ -4,9 +4,10 @@ import { ExtractJwt, Strategy } from "passport-jwt";
 import { ConfigService } from "@nestjs/config";
 import { UserService } from "src/user/user.service";
 import { User } from "@prisma/client";
+import { t_get_one_fields } from "src/user/alias";
 
-type t_payload = { 
-	sub: string | undefined 
+type t_payload = {
+	sub: string | undefined;
 };
 
 @Injectable()
@@ -26,12 +27,10 @@ export class JwtStrategy extends PassportStrategy(Strategy, "jwt") {
 	// as its single parameter. Based on the way JWT signing
 	// works, we're guaranteed that we're receiving a valid token
 	// that we have previously signed and issued to a valid user.
-	async validate(payload: t_payload): Promise<User> {
-		let user: User;
-		if (payload.sub !== undefined)
-			user = await this._user.get_one(payload.sub, payload.sub);
-		else
-			throw new Error("Unvalid payload sent from JWT signing");
+	async validate(payload: t_payload): Promise<t_get_one_fields> {
+		let user: t_get_one_fields;
+		if (payload.sub !== undefined) user = await this._user.get_one(payload.sub, payload.sub);
+		else throw new Error("Unvalid payload sent from JWT signing");
 		return user;
 	}
 }
