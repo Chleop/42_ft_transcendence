977654b4024e89776716b683062eb22e7ea88c9a
Nils Mathieu <nils.mathieu.contact@gmail.com>
2023-01-31 13:20:53 +0100

    spectator setup

--- frontend/src/game/index.ts
+++ frontend/src/game/index.ts
@@ -1,4 +1,5 @@
 export * from "./constants";
 export * from "./ongoing_game";
 export * from "./playing_game";
+export * from "./ongoing_game";
 export * from "./game_board";
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,8 @@
 import { History } from "./strawberry";
 import { RouteData, Router } from "./strawberry/router";
 import { MainMenu } from "./main_menu/main_menu";
+import { GameBoard } from "./game";
+import { SpectatingGame } from "./game/spectating_game";
 
 /**
  * The entry point of the application.
@@ -9,14 +11,18 @@ function entry_point() {
 	const router = new Router<(data: RouteData) => void>();
 
     router.register_route("/", () => History.replace_state(MainMenu));
-	router.register_route("/spectate/:room_id", () => History.replace_state(MainMenu));
+	router.register_route("/spectate/:room_id", data => {
+		GameBoard.start_game(new SpectatingGame(data["room_id"]));
+		History.replace_state(GameBoard);
+	});
 
 	const route_result = router.get(window.location.pathname);
 	if (route_result) {
 		route_result.meta(route_result.data);
 	} else {
-		document.body.innerText = "lol c'est nul part.";
-		document.body.style.color = "white";
+		// When the page is not found,
+		// TP the user on the main menu.
+		History.replace_state(MainMenu);
 	}
 }
 
