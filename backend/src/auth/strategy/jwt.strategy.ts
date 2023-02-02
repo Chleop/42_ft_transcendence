c0a9a62efac7ade95241188e8d8ef6d180bf15f5
Chleo PROESCHEL <cproesch@e3r10p9.42.fr>
2023-02-02 19:08:30 +0100

    optimised

--- backend/src/auth/strategy/jwt.strategy.ts
+++ backend/src/auth/strategy/jwt.strategy.ts
@@ -1,49 +1,49 @@
-import { Injectable, InternalServerErrorException, UnauthorizedException } from "@nestjs/common";
+import {
+	Injectable,
+	InternalServerErrorException,
+	Logger,
+	UnauthorizedException,
+} from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
 import { ConfigService } from "@nestjs/config";
-import { t_user_obj } from "src/auth/alias";
 import { UserNotFoundError } from "src/user/error";
-import { UserService } from "src/user/user.service";
-import { t_get_one_fields } from "src/user/alias";
-
-type t_payload = { sub: string };
+import { AuthController } from "../auth.controller";
+import { t_payload } from "../alias";
+import { AuthService } from "../auth.service";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, "jwt") {
-	private _user: UserService;
+	public readonly _logger: Logger;
+	public readonly _authService: AuthService;
 
-	constructor(config: ConfigService) {
+	constructor() {
+		const _config = new ConfigService();
 		super({
 			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
 			ignoreExpiration: false,
-			secretOrKey: config.get("JWT_SECRET"),
+			secretOrKey: _config.get("JWT_SECRET"),
 		});
-		this._user = new UserService();
+		this._logger = new Logger(AuthController.name);
+		this._authService = new AuthService();
 	}
 
 	// Passport invokes the validate() method passing the decoded JSON
 	// as its single parameter. Based on the way JWT signing
 	// works, we're guaranteed that we're receiving a valid token
 	// that we have previously signed and issued to a valid user.
-	public async validate(payload: t_payload): Promise<t_user_obj> {
+	public async validate(payload: t_payload): Promise<any> {
+		this._logger.debug("IN JwtStrategy validate function");
 		try {
-			const user: t_get_one_fields = await this._user.get_one(payload.sub, payload.sub);
-			let user_obj: t_user_obj;
-			user_obj = {
-				id: user.id,
-				login: user.login,
-				name: user.name,
-				email: user.email,
-				twoFactAuth: user.twoFactAuth,
-			};
-			return user_obj;
+			const user = await this._authService.get_user_auth(payload.sub);
+			this._logger.debug("IN JwtStrategy validate function: user is found : " + user.id);
+			return user;
 		} catch (error) {
 			if (error instanceof UserNotFoundError) {
-				console.log(error.message);
+				this._logger.error(error.message);
 				throw new UnauthorizedException(error.message);
 			}
-			console.log("Unknown error type, this should not happen");
+			this._logger.error("Unknown error type, this should not happen");
 			throw new InternalServerErrorException();
 		}
 	}
