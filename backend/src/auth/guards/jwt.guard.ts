80934f21ec4f03860877f7789e49802c8cf38ee7
Chleo PROESCHEL <cproesch@e3r10p9.42.fr>
2023-02-02 19:09:23 +0100

    handle request not needed

--- backend/src/auth/guards/jwt.guard.ts
+++ backend/src/auth/guards/jwt.guard.ts
@@ -1,11 +1,5 @@
-import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { Injectable } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 
 @Injectable()
-export class JwtGuard extends AuthGuard("jwt") {
-	public override handleRequest<TUser = any>(err: any, user: any): TUser {
-		if (err) throw err;
-		else if (!user) throw new UnauthorizedException();
-		return user;
-	}
-}
+export class JwtGuard extends AuthGuard("jwt") {}
