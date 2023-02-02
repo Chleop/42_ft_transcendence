ae43862528a61d850b3d04d724bb6da2efd8aef7
Chleo PROESCHEL <cproesch@e3r10p9.42.fr>
2023-02-02 19:10:19 +0100

    created aliases for auth

--- backend/src/auth/alias/index.ts
+++ backend/src/auth/alias/index.ts
@@ -1,1 +1,5 @@
-export * from "./request_user_obj.alias";
+export * from "./user_auth.alias";
+export * from "./email.alias";
+export * from "./secret_auth.alias";
+export * from "./payload.alias";
+export * from "./access_token.alias";
--- backend/src/auth/alias/request_user_obj.alias.ts
+++
@@ -1,7 +0,0 @@
-export type t_user_obj = {
-	id: string;
-	login: string;
-	name: string;
-	email: string | null;
-	twoFactAuth: boolean;
-};
