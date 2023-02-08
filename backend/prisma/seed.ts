a16282b7e694c5b5067565a39bf870de2602f352
Chleo PROESCHEL <cproesch@e3r10p9.42.fr>
2023-02-08 18:27:19 +0100

    modified prisma skin model

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -18,8 +18,9 @@ async function main() {
 	const skin_id: string = (
 		await prisma.skin.create({
 			data: {
-				name: "Default",
-				url: "resource/skin/default.jpg",
+				background: "resource/skin/background/default.jpg",
+				ball: "resource/skin/ball/default.jpg",
+				paddle: "resource/skin/paddle/default.jpg",
 			},
 		})
 	).id;
