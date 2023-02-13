021c0aaa8158eb211a489e868fa6b2f47e2b85ff
Nils Mathieu <nils.mathieu.contact@gmail.com>
2023-02-13 18:17:55 +0100

    removed wildcard

--- backend/src/socket-io.adapter.ts
+++ backend/src/socket-io.adapter.ts
@@ -47,7 +47,6 @@ export class SocketIOAdapter extends IoAdapter {
 
 		const cors: CorsOptions = {
 			origin: [
-				"*",
 				`http://localhost:${client_port}`,
 				new RegExp(`/^http:\/\/192\.168\.1\.([1-9]|[1-9]\d):${client_port}$/`),
 			],
