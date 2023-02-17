0e1d907a85c3ff006dd3a4bebdf6ad7952e5e98e
Jonathan DUFOUR <jodufour@student.42.fr>
2023-02-17 02:00:22 +0100

    Update the object type returned as reponse of the HTTP route for joining a channel [OK]

---
+++ backend/src/channel/interface/channel.interface.ts
@@ -0,0 +1,33 @@
+import { ChanType } from "@prisma/client";
+
+export interface IChannel {
+	id: string;
+	name: string;
+	type: ChanType;
+	owner_id: string | null;
+	members_count: number;
+	operators_ids: string[];
+}
+
+export interface IChannelTmp {
+	id: string;
+	name: string;
+	chanType: ChanType;
+	hash: string | null;
+	ownerId: string | null;
+	members: IMember[];
+	operators: IOperator[];
+	banned: IBanned[];
+}
+
+interface IMember {
+	id: string;
+}
+
+interface IOperator {
+	id: string;
+}
+
+interface IBanned {
+	id: string;
+}
