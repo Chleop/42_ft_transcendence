742229d7027c0bd85ac02e27ab17279db0f07865
Jonathan DUFOUR <jodufour@student.42.fr>
2023-02-20 14:07:43 +0100

    Remove the `channels` field from the `IUserPublic` interface [OK] Add the `channels` field to the `IUserPrivate` interface     [OK]

--- backend/src/user/interface/user_private.interface.ts
+++ backend/src/user/interface/user_private.interface.ts
@@ -1,10 +1,12 @@
 import { IUserPublic, IUserPublicTmp } from "src/user/interface";
 import { IGame, IGameTmp } from "src/game/interface";
+import { IChannel, IChannelTmp } from "src/channel/interface";
 
 export interface IUserPrivate extends IUserPublic {
 	login: string;
 	email: string | null;
 	two_fact_auth: boolean;
+	channels: IChannel[];
 	channels_owned_ids: string[];
 	games_played: IGame[];
 	friends_ids: string[];
@@ -16,6 +18,7 @@ export interface IUserPrivateTmp extends IUserPublicTmp {
 	login: string;
 	email: string | null;
 	twoFactAuth: boolean;
+	channels: IChannelTmp[];
 	channelsOwned: IChannelOwned[];
 	gamesPlayed: IGameTmp[];
 	friends: IFriend[];
